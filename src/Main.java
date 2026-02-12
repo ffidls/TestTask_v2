54f0cd1f122c9cf2af7c192c531f2fd9d452424d
alinaorekhova <alina.orekhova@kofax.com>
2026-02-11 20:13:30 -0800

    ь

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,8 @@ public static void main(String[] args) {
     Node num = new NumberNode(10);
     Node var = new VariableNode("x");
 
-    // 2. Собираем их в Плюс
     Node sum = new AddNode(num, var);
 
-    // 3. Считаем всё дерево сразу
     System.out.println("Результат 10 + x: " + sum.evaluate(myVars)); // Будет 15
 
     System.out.println("Вид дерева:");
